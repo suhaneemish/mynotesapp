2ba8a408c1f40a759a25f3d66274d0c3f648daff
Suhani Mishra <141520981+suhaneemish@users.noreply.github.com>
2024-07-06 11:10:38 +0530

    Update Footer.jsx

--- Footer.jsx
+++ Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Made by Shikha Raj</p>
+      <p>Made by Suhani Mishra</p>
       <p>Copyright @ {year}</p> 
     </footer>
   );
